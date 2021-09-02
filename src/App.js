bda42f3d14d40457e1d1308bf54116d2c90e020d
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-09-03 02:19:13 +0600

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
+import Signup from './comps/Signup';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
+import { Container } from "react-bootstrap"
 
 function App() {
   const [selectedImage, setSelectedImage] = useState()
   return (
+    
     <div className="App">
+      <Container
+    className="d-flex align-items-center justify-content-center"
+    style={{ minHeight: "100vh" }}
+  >
+     <div className="w-100" style={{ maxWidth: "400px" }}>
+      <Signup />
+      </div>
+      </Container>
       <Title/>
       <UploadForm />
       <ImageGrid setSelectedImage={setSelectedImage} />
@@ -15,6 +26,7 @@ function App() {
       <Modal selectedImage={selectedImage} 
       setSelectedImage={setSelectedImage}/>)}
     </div>
+    
   );
 }
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAOvjSUzbSQwo-Do3FEcKCSTVskQLIVea4",
@@ -15,6 +16,7 @@ const firebaseConfig = {
 
   const projectStorage = firebase.storage();
   const projectFirestore = firebase.firestore();
+  const auth = firebase.auth();
   const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-  export { projectStorage, projectFirestore, timestamp }
+  export { projectStorage, projectFirestore,auth, timestamp }
